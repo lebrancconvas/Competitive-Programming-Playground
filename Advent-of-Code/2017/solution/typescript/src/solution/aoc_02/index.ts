08a92c4439fbcf58949c32fe07127ee431f81c2c
lebrancconvas <lebrancconvas@gmail.com>
2025-01-10 23:28:09 +0700

    ✨ feat(aoc): update solution for aoc_2017_02 using typescript

--- Advent-of-Code/2017/solution/typescript/src/solution/aoc_02/index.ts
+++ Advent-of-Code/2017/solution/typescript/src/solution/aoc_02/index.ts
@@ -1,5 +1,6 @@
 import { getInput } from "../../helper/input";
-import { findMinAndMax } from "./utils";
+import { findMinAndMax, findDivider } from "./utils";
+
 
 export function aoc_02_1() {
   const input = getInput('aoc_02').trim();
@@ -18,8 +19,21 @@ export function aoc_02_1() {
 };
 
 export function aoc_02_2() {
+  const input = getInput('aoc_02');
+
+  const rows = input.split('\n');
+  const table = rows.map(row => row.trim().split('\t').map(data => parseInt(data.trim())));
+
+  let result = 0;
+
+  for(let i = 0; i < rows.length; i++) {
+    result += findDivider(table[i]);
+  }
+
+  return result;
 
 }
 
 
 console.log(aoc_02_1());
+console.log(aoc_02_2());
--- Advent-of-Code/2017/solution/typescript/src/solution/aoc_02/utils.ts
+++ Advent-of-Code/2017/solution/typescript/src/solution/aoc_02/utils.ts
@@ -1,3 +1,4 @@
+// Part 1
 export function findMinAndMax(nums: number[]): [number, number] {
   let min = Infinity;
   let max = 0;
@@ -8,5 +9,20 @@ export function findMinAndMax(nums: number[]): [number, number] {
   }
 
   return [min, max];
+};
+
+// Part 2
+
+export function findDivider(nums: number[]): number {
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = i + 1; j < nums.length; j++) {
+      if(nums[i] % nums[j] === 0 || nums[j] % nums[i] === 0) {
+        if(nums[i] > nums[j]) return nums[i] / nums[j];
+        else return nums[j] / nums[i];
+      }
+    }
+  }
+
+  return 0;
 
 };
