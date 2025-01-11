82706281916bcedc92c96f7b0ee0baf672ead99e
lebrancconvas <lebrancconvas@gmail.com>
2025-01-11 13:29:44 +0700

    ✨ feat(aoc): update solution for aoc_2017_08 part 1 using typescript

---
+++ Advent-of-Code/2017/solution/typescript/src/solution/aoc_08/index.ts
@@ -0,0 +1,113 @@
+import { register } from "module";
+import { getInput } from "../../helper/input";
+
+export function aoc_08_1() {
+
+  const input = getInput('aoc_08');
+
+  const registers = new Map<string, number>();
+
+  const rows = input.trim().split('\n').map(instruction => instruction.trim().split(' ').filter(x => x !== 'if'));
+
+  for(let i = 0; i < rows.length; i++) {
+    const [mainVar, operator, rawOperand, compVar, comparator, rawComparand] = rows[i];
+
+    const operand = parseInt(rawOperand);
+    const comperand = parseInt(rawComparand);
+
+    if(!registers.has(mainVar)) {
+      registers.set(mainVar, 0);
+    }
+    if(!registers.has(compVar)) {
+      registers.set(compVar, 0);
+    }
+
+    const compVarValue = registers.get(compVar);
+
+    let mainVarValue = registers.get(mainVar);
+
+    if(typeof compVarValue === 'number' && typeof mainVarValue === 'number') {
+      switch(comparator) {
+      case '>':
+        if(compVarValue > comperand) {
+          if(operator === 'inc') {
+            mainVarValue += operand;
+            registers.set(mainVar, mainVarValue);
+          } else if(operator === 'dec') {
+            mainVarValue -= operand;
+            registers.set(mainVar, mainVarValue);
+          }
+        }
+        break;
+      case '<':
+        if(compVarValue < comperand) {
+          if(operator === 'inc') {
+            mainVarValue += operand;
+            registers.set(mainVar, mainVarValue);
+          } else if(operator === 'dec') {
+            mainVarValue -= operand;
+            registers.set(mainVar, mainVarValue);
+          }
+        }
+        break;
+      case '>=':
+        if(compVarValue >= comperand) {
+          if(operator === 'inc') {
+            mainVarValue += operand;
+            registers.set(mainVar, mainVarValue);
+          } else if(operator === 'dec') {
+            mainVarValue -= operand;
+            registers.set(mainVar, mainVarValue);
+          }
+        }
+        break;
+      case '<=':
+        if(compVarValue <= comperand) {
+          if(operator === 'inc') {
+            mainVarValue += operand;
+            registers.set(mainVar, mainVarValue);
+          } else if(operator === 'dec') {
+            mainVarValue -= operand;
+            registers.set(mainVar, mainVarValue);
+          }
+        }
+        break;
+      case '==':
+        if(compVarValue == comperand) {
+          if(operator === 'inc') {
+            mainVarValue += operand;
+            registers.set(mainVar, mainVarValue);
+          } else if(operator === 'dec') {
+            mainVarValue -= operand;
+            registers.set(mainVar, mainVarValue);
+          }
+        }
+        break;
+      case '!=':
+        if(compVarValue != comperand) {
+          if(operator === 'inc') {
+            mainVarValue += operand;
+            registers.set(mainVar, mainVarValue);
+          } else if(operator === 'dec') {
+            mainVarValue -= operand;
+            registers.set(mainVar, mainVarValue);
+          }
+        }
+        break;
+      }
+    }
+
+  }
+
+  let registerValues = [...registers.values()];
+
+  let result = Math.max(...registerValues);
+
+  return result;
+};
+
+export function aoc_08_2() {
+
+};
+
+console.log(aoc_08_1());
